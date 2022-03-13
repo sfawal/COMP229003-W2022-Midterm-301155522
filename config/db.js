dd79b305d6a8144bc44bfdf49f8036a2415d1c16
sfawal <sfawal@my.centennialcollege.ca>
2022-03-12 21:39:57 -0500

    1.0

--- config/db.js
+++ config/db.js
@@ -1,3 +1,20 @@
-module.exports = {
-    "AtlasDB": ""
+// For production environment never expose your connection string.
+let URI = "mongodb+srv://dbadmin:ylR8Z4WxKDrk2Bl5@clusters003.amfry.mongodb.net/mydb?retryWrites=true&w=majority"
+
+//Database setup
+let mongoose = require('mongoose');
+
+module.exports = function(){
+
+    // Connect to the Database
+    mongoose.connect(URI);
+
+    let mongoDB = mongoose.connection;
+   
+    mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+    mongoDB.once('open', ()=>{
+        console.log('Connected to MongoDB...');
+    });
+
+    return mongoDB;
 }
